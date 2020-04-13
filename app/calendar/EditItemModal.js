1adbbd74c0fca8a5efc3b0d0c38c0d4c5a8fc7bd
Oleg Smushkevich <olegs777@bezeqint.net>
2020-04-13 12:21:59 +0300

    ok

--- app/calendar/EditItemModal.js
+++ app/calendar/EditItemModal.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet, TextInput, Modal} from 'react-native';
 import Button from '../components/Button';
 import NumericInput from 'react-native-numeric-input';
+// import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import moment from 'moment';
 
 import {connect} from 'react-redux';
 
 export class EditItemsModal extends Component {
   state = {
     visible: false,
-    timeStart: '',
-    timeEnd: '',
+    timeStart: '00:00',
+    timeEnd: '00:00',
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.showModal !== this.props.showModal) {
@@ -20,8 +22,10 @@ export class EditItemsModal extends Component {
   }
   componentDidMount() {
     this.setState({
-      timeStart: this.props.item.timeStart,
-      timeEnd: this.props.item.timeEnd,
+      timeStart: this.props.item.timeStart
+        ? moment(this.props.item.timeStart).format('HH:mm')
+        : '00:00',
+      timeEnd: this.props.item.timeEnd ? this.props.item.timeEnd : '00:00',
     });
   }
 
@@ -37,8 +41,144 @@ export class EditItemsModal extends Component {
       <Modal visible={this.state.visible}>
         <View style={styles.container}>
           <Text style={styles.textTitle}>Edit Job Time</Text>
+
           <View style={styles.start}>
-            <Text>Start</Text>
+            <View //Header Start
+              style={{
+                flexDirection: 'row',
+                borderWidth: 1,
+                marginBottom: 10,
+                justifyContent: 'space-between',
+                alignSelf: 'center',
+                width: '30%',
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                  marginVertical: 10,
+                }}>
+                Start :
+              </Text>
+              <View
+                style={{borderWidth: 1, paddingHorizontal: 10, paddingTop: 10}}>
+                <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                  {this.state.timeStart}
+                </Text>
+              </View>
+            </View>
+            <View //Hours Start
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: '90%',
+                alignSelf: 'center',
+                marginBottom: 10,
+              }}>
+              <Text style={{fontSize: 18, marginRight: 16}}>Hours</Text>
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+            </View>
+            <View //Minutes Start
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: '90%',
+                alignSelf: 'center',
+              }}>
+              <Text style={{fontSize: 18}}>Minutes</Text>
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+            </View>
+            <View style={{marginVertical: 5, alignItems: 'center'}}>
+              <Button text="Submit" onPress={this._timeStart} />
+            </View>
+          </View>
+          <View style={styles.end}>
+            <View //Header Start
+              style={{
+                flexDirection: 'row',
+                borderWidth: 1,
+                marginBottom: 10,
+                justifyContent: 'space-between',
+                alignSelf: 'center',
+                width: '30%',
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                  marginVertical: 10,
+                }}>
+                End:
+              </Text>
+              <View
+                style={{borderWidth: 1, paddingHorizontal: 10, paddingTop: 10}}>
+                <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                  {this.state.timeEnd}
+                </Text>
+              </View>
+            </View>
+            <View //Hours Start
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: '90%',
+                alignSelf: 'center',
+                marginBottom: 10,
+              }}>
+              <Text style={{fontSize: 18, marginRight: 16}}>Hours</Text>
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+            </View>
+            <View //Minutes Start
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: '90%',
+                alignSelf: 'center',
+              }}>
+              <Text style={{fontSize: 18}}>Minutes</Text>
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+              <NumericInput
+                value={this.state.value}
+                onChange={value => this.setState({value})}
+                type="up-down"
+              />
+            </View>
+            <View style={{marginVertical: 5, alignItems: 'center'}}>
+              <Button text="Submit" onPress={this._timeEnd} />
+            </View>
           </View>
         </View>
         <Button text="close" onPress={this._closeModal} />
@@ -61,12 +201,22 @@ export default connect(
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderWidth: 3,
+    paddingTop: 10,
+    alignContent: 'center',
   },
   textTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    borderWidth: 1,
     textAlign: 'center',
+  },
+  start: {
+    borderWidth: 1,
+    borderColor: 'green',
+    paddingVertical: 20,
+  },
+  end: {
+    borderWidth: 1,
+    borderColor: 'green',
+    paddingVertical: 20,
   },
 });
