389740f685382ae85b37d5f4163d76b217e38dd8
olegs7771 <olegs7771@gmail.com>
2020-04-15 01:26:41 +0300

    ok

--- app/calendar/EditItemModal.js
+++ app/calendar/EditItemModal.js
@@ -1,17 +1,14 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, TextInput, Modal} from 'react-native';
+import {View, Text, StyleSheet, Modal} from 'react-native';
 import Button from '../components/Button';
-import NumericInput from 'react-native-numeric-input';
-import Icon from 'react-native-vector-icons/dist/FontAwesome';
-import moment from 'moment';
 
 import {connect} from 'react-redux';
+import EditStartTime from './EditStartTime';
+import EditEndTime from './EditEndTime';
 
 export class EditItemsModal extends Component {
   state = {
     visible: false,
-    hours: '00',
-    minutes: '00',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -25,39 +22,12 @@ export class EditItemsModal extends Component {
     this.props.closeModal();
     this.setState({visible: false});
   };
+
   render() {
-    const {hours, minutes} = this.state;
     return (
       <Modal visible={this.state.visible}>
-        <View style={styles.container}>
-          <Text style={styles.textTitle}>Edit End Time</Text>
-          <View style={styles.showTime}>
-            <Text style={{fontSize: 20}}>{`${hours}:${minutes}`}</Text>
-          </View>
-
-          <View style={styles.editTime}>
-            <View style={styles.hours}>
-              <Text
-                style={{textAlign: 'center', fontSize: 18, marginVertical: 5}}>
-                Hours
-              </Text>
-              <NumericInput
-                value={this.state.value}
-                onChange={this._hoursEdit}
-              />
-            </View>
-            <View style={styles.minutes}>
-              <Text
-                style={{textAlign: 'center', fontSize: 18, marginVertical: 5}}>
-                Minutes
-              </Text>
-              <NumericInput
-                value={this.state.value}
-                onChange={this._minutesEdit}
-              />
-            </View>
-          </View>
-        </View>
+        <EditStartTime />
+        <EditEndTime />
         <Button text="Close" onPress={this._closeModal} />
       </Modal>
     );
@@ -74,40 +44,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(EditItemsModal);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 10,
-    alignContent: 'center',
-    paddingTop: 20,
-    alignItems: 'center',
-  },
-  textTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginVertical: 10,
-  },
-  editTime: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    justifyContent: 'space-between',
-    marginVertical: 20,
-    width: '60%',
-  },
-  showTime: {
-    borderWidth: 1,
-    borderColor: 'green',
-    marginVertical: 10,
-    alignSelf: 'center',
-  },
-  hours: {
-    borderWidth: 1,
-    borderColor: 'red',
-  },
-  minutes: {
-    borderWidth: 1,
-    borderColor: 'red',
-  },
-});
---
+++ app/calendar/EditStartTime.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Button from '../components/Button';
+import NumericInput from 'react-native-numeric-input';
+import moment from 'moment';
+import {connect} from 'react-redux';
+
+export class EditStartTime extends Component {
+  state = {
+    hours: 0,
+    minutes: 0,
+    isDoubleDIgitHours: false,
+    isDoubleDIgitMinutes: false,
+  };
+  //Hide 0 if double digit
+  _hoursEdit = e => {
+    if (e > 8) {
+      this.setState({isDoubleDIgitHours: true});
+    }
+    if (e < 10) {
+      this.setState({isDoubleDIgitHours: false});
+    }
+    this.setState({hours: e});
+  };
+  _minutesEdit = e => {
+    if (e > 8) {
+      this.setState({isDoubleDIgitMinutes: true});
+    }
+    if (e < 10) {
+      this.setState({isDoubleDIgitMinutes: false});
+    }
+    this.setState({minutes: e});
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.hours !== this.state.hours) {
+      this.setState({hours: this.state.hours});
+    }
+    if (prevState.minutes !== this.state.minutes) {
+      this.setState({minutes: this.state.minutes});
+    }
+  }
+
+  render() {
+    const {hours, minutes} = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textTitle}>Edit Start Time</Text>
+        <View style={styles.showTime}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+            {this.state.isDoubleDIgitHours ? `${hours}` : `0${hours}`}:
+            {this.state.isDoubleDIgitMinutes ? `${minutes}` : `0${minutes}`}
+          </Text>
+        </View>
+
+        <View style={styles.editTime}>
+          <View style={styles.hours}>
+            <View style={{borderWidth: 1, marginBottom: 20}}>
+              <Text style={{textAlign: 'center', fontSize: 18}}>Hours</Text>
+            </View>
+            <NumericInput
+              value={this.state.hours}
+              onChange={this._hoursEdit}
+              maxValue={23}
+              minValue={0}
+              onLimitReached={(isMax, msg) => console.log(isMax, msg)}
+              rounded={true}
+              totalHeight={50}
+              totalWidth={150}
+            />
+          </View>
+          <View style={styles.minutes}>
+            <View style={{borderWidth: 1, marginBottom: 20}}>
+              <Text style={{textAlign: 'center', fontSize: 18}}>Minutes</Text>
+            </View>
+            <NumericInput
+              value={this.state.value}
+              onChange={this._minutesEdit}
+              maxValue={59}
+              minValue={0}
+              rounded={true}
+              totalHeight={50}
+              totalWidth={150}
+            />
+          </View>
+        </View>
+        <View>
+          <Button text="Submit" />
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(EditStartTime);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 10,
+    alignContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  textTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  editTime: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+    width: '80%',
+  },
+  showTime: {
+    marginVertical: 10,
+    alignSelf: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+});
