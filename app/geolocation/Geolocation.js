154c1a145900caff87ea5b333b2fe871a01544eb
Oleg Smushkevich <olegs777@bezeqint.net>
2020-05-21 13:13:58 +0300

    ok

--- app/geolocation/Geolocation.js
+++ app/geolocation/Geolocation.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import RequestGeoLocPermission from './GeoPermission';
-import Geo from '@react-native-community/geolocation';
+// import Geo from '@react-native-community/geolocation';
+import Geoloc from 'react-native-geolocation-service';
+import Geocoder from 'react-native-geocoding';
+Geocoder.init('AIzaSyDF9BWn17CT9geI3L-Ff0ujGWwpPHmxvCQ');
 
 class Geolocation extends Component {
   state = {
     coords: {},
+    address: '',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -31,7 +35,7 @@ class Geolocation extends Component {
       //Got Permission from GeoPermission
       if (permission) {
         if (this.props.projectCoords) {
-          Geo.watchPosition(
+          Geoloc.watchPosition(
             position => {
               console.log('position in cdm', position);
               this.setState(prevState => ({
@@ -41,14 +45,26 @@ class Geolocation extends Component {
                   lng: position.coords.longitude,
                 },
               }));
+              //GeoDecode position
+              Geocoder.from(position.coords.latitude, position.coords.longitude)
+                .then(address => {
+                  console.log('address', address);
+                  this.setState(prevState => ({
+                    ...prevState,
+                    address: address.results[0].formatted_address,
+                  }));
+                })
+                .catch(err => {
+                  console.log('error to decode', err);
+                });
             },
             error => {
               // See error code charts below.
               console.log(error.code, error.message);
             },
             {
-              enableHighAccuracy: true,
-
+              enableHighAccuracy: false,
+              distanceFilter: 10,
               timeout: 15000,
               maximumAge: 10000,
             },
